4fbc8e891d96c829ab872df59697b168e2c7c3a5
nitaigochmanhareven-maker <nitai.gochman.hareven@gmail.com>
2025-11-07 15:35:14 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,8 +1,6 @@
 def find_max_number(num1, num2, num3):
-    if num1 > num2 and  num1 >= num3:
-        return num1
-    elif num2 >= num1 and num2 > num3:
+    if num1 >= num2 and  num1 >= num3:
+      return num1
+    elif num2 >= num1 and num2 >= num3:
         return num2
-    else:
-        return num3
-
+    return num3
